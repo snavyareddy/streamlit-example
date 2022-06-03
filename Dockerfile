fa7fd3673d9bdabba4bfc3651a6d93b28d8834f9
snavyareddy <santhapurinavya1999@gmail.com>
2022-06-03 17:20:10 +0530

    streamlit-test

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.7
+
+COPY environment_setup/requirements.txt ./requirements.txt
+RUN pip install -r requirements.txt
+
+COPY /.streamlit/config.toml /.streamlit/config.toml
+
+COPY . .
+
+EXPOSE 80
+
+ENTRYPOINT ["streamlit", "run"]
+CMD ["src/streamlit_script/word_cloud.py"]
---
+++ src/streamlit_script/word_cloud.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import math
+import spacy
+import base64
+import os
+import sys
+import time
+import re
+import json
+path = os.getcwd()
+sys.path.append(path)
+from src.utils.helper_function import *
+# Libraries for worcloud
+import nltk
+from wordcloud import WordCloud, STOPWORDS
+from PyDictionary import PyDictionary
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import matplotlib.pyplot as plt
+nltk.download('punkt')
+
+def convert_df(df):
+    # return df.to_csv().encode('utf-8')
+    return df.to_csv()
+
+
+
+html_temp = """
+<div style ="background-color:lightblue;padding:13px">
+<h1 style ="color:black;text-align:center;">Streamlit-example</h1>
+</div>
+"""
+st.write(html_temp, unsafe_allow_html=True)
+st.write("#")
+
+if uploaded_file is not None:
+    try:
+        df1 = pd.read_csv(uploaded_file)
+    except Exception:
+        df1 = pd.read_excel(uploaded_file)
+    st.components.v1.html("""
+<h1 style ="color:black;text-align:center">Preview of Data from Input File</h1>
+""", height=60)
+    st.dataframe(df1)
+    my_data = convert_df(df1)
+    col = df1.select_dtypes(['object']).columns
+    st.write("----")
